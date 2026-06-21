5d8a1613569398c8e106673b6a93ca40d254af60
Developer <developer@cdut.com>
2026-06-21 16:56:24 +0800

    chore: 脚手架迁移到 uni-app vue3-ts 工程

---
+++ learn-mobi/src/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png" />
+    <text class="title">{{ title }}</text>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const title = ref('智慧教育 - 学员端')
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.logo {
+  width: 200rpx;
+  height: 200rpx;
+  margin-bottom: 40rpx;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #333;
+}
+</style>
